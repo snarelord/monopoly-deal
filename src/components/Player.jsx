335a487f9dd6947ea810a6e8600411abdf9bd8ab
Kit Jones <174150846+snarelord@users.noreply.github.com>
2025-01-29 18:34:29 +0000

    added card data

---
+++ src/components/Player.jsx
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+
+// player default starts with 10m and empty array of properties
+const Player = () => {
+  const [money, setMoney] = useState(10);
+  const [properties, setProperties] = useState([]);
+};
