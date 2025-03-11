220b90d8e47031d97507bb48ea8dacb751e39691
Kit Jones <174150846+snarelord@users.noreply.github.com>
2025-03-11 16:31:40 +0000

    added card data and component files

---
+++ src/lib/types.ts
@@ -0,0 +1,46 @@
+export interface Card {
+  id: string;
+  type: "property" | "money" | "action" | "wildcard";
+  name: string;
+  value: number;
+  color?: string;
+  secondaryColor?: string; // two-way wildcards
+  actionType?: string;
+  image: string;
+}
+
+export interface PropertySet {
+  color: string;
+  cards: Card[];
+  isComplete: boolean;
+  houses: number;
+  hotels: number;
+  requiredCards: number;
+}
+
+export interface Player {
+  id: number;
+  hand: Card[];
+  bank: Card[];
+  properties: PropertySet[];
+}
+
+export interface ActionInProgress {
+  type: string;
+  sourcePlayer: number;
+  targetPlayer?: number;
+  card: Card;
+}
+
+export interface GameState {
+  players: Player[];
+  deck: Card[];
+  currentPlayerIndex: number;
+  cardsPlayed: number;
+  isGameOver: boolean;
+  winner: Player | null;
+  actionInProgress: ActionInProgress | null;
+  hasDrawnCards: boolean;
+  actionCards: Card[];
+  message?: string;
+}
