1a16dff7cffd6272a81dddc45c7f157e0c1678ba
Kit Jones <174150846+snarelord@users.noreply.github.com>
2025-03-13 18:49:46 +0000

    fixed black property issue

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -161,7 +161,7 @@
 /******/ 		// This function allow to reference async chunks
 /******/ 		__webpack_require__.u = (chunkId) => {
 /******/ 			// return url for filenames based on template
-/******/ 			return undefined;
+/******/ 			return "static/chunks/" + chunkId + ".js";
 /******/ 		};
 /******/ 	})();
 /******/ 	
@@ -190,7 +190,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("f31e21d1ecf1ef0e")
+/******/ 		__webpack_require__.h = () => ("ed77a4578ba82d0a")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
